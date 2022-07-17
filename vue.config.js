8cf705edbc5add50b85ae7719cffd0d9bc7d8487
youngguyz <2281300996@qq.com>
2022-07-17 12:01:36 +0800

    :rocket:修改代码

--- vue.config.js
+++ vue.config.js
@@ -35,8 +35,8 @@ module.exports = {
     overlay: {
       warnings: false,
       errors: true
-    },
-    before: require('./mock/mock-server.js')
+    }
+    // before: require('./mock/mock-server.js') 注释掉
   },
   configureWebpack: {
     // provide the app's title in webpack's name field, so that
